f1a7a99dbecc0bc2284c219943642b7f5906fd1d
deji-dd <dejib50@gmail.com>
2023-11-04 16:38:53 +0100

    back again

--- functions/career/submit.js
+++ functions/career/submit.js
@@ -6,50 +6,50 @@ export async function onRequestPost(context) {
     let cover = data.get("cover");
     let resume = data.get("resume");
 
-    // await fetch(
-    //   new Request("https://api.mailchannels.net/tx/v1/send", {
-    //     method: "POST",
-    //     headers: {
-    //       "content-type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       personalizations: [
-    //         {
-    //           to: [
-    //             {
-    //               email: "president@gloriouseagles.com",
-    //               name: "Glorious Eagles LLC",
-    //             },
-    //           ],
-    //         },
-    //       ],
-    //       from: {
-    //         email: "president@gloriouseagles.com",
-    //         name: "Glorious Eagles LLC",
-    //       },
-    //       subject: "New Application",
-    //       content: [
-    //         {
-    //           type: "text/html",
-    //           value:
-    //             "<body><p>Name: " +
-    //             name +
-    //             "</p><br /><p>Email: " +
-    //             email +
-    //             "</p><br/><p>Cover Letter: <a href='" +
-    //             cover +
-    //             "'>" +
-    //             cover +
-    //             "</a></p><br/><p>Resume: <a href='" +
-    //             resume +
-    //             "'>" +
-    //             resume +
-    //             "</a></p></body>",
-    //         },
-    //       ],
-    //     }),
-    //   })
-    // );
+    await fetch(
+      new Request("https://api.mailchannels.net/tx/v1/send", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          personalizations: [
+            {
+              to: [
+                {
+                  email: "president@gloriouseagles.com",
+                  name: "Glorious Eagles LLC",
+                },
+              ],
+            },
+          ],
+          from: {
+            email: "president@gloriouseagles.com",
+            name: "Glorious Eagles LLC",
+          },
+          subject: "New Application",
+          content: [
+            {
+              type: "text/html",
+              value:
+                "<body><p>Name: " +
+                name +
+                "</p><br /><p>Email: " +
+                email +
+                "</p><br/><p>Cover Letter: <a href='" +
+                cover +
+                "'>" +
+                cover +
+                "</a></p><br/><p>Resume: <a href='" +
+                resume +
+                "'>" +
+                resume +
+                "</a></p></body>",
+            },
+          ],
+        }),
+      })
+    );
 
     await fetch(
       new Request("https://api.mailchannels.net/tx/v1/send", {
