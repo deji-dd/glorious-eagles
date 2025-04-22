a742c49d43588a0a703ff6932f5428248a8fef04
deji-dd <dejib50@gmail.com>
2025-04-22 22:49:43 +0100

    Add CORS headers and cleanup form fields

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -43,7 +43,7 @@ export default function Socials() {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-printer"
+          className="bi bi-printer"
           viewBox="0 0 16 16"
         >
           <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
